30e70c7203b89dde02667fe039173a7e21588a93
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2026-05-11 20:23:02 +0300

    Added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://ombersigorta.com",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
